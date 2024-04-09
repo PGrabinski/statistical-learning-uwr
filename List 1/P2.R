05d192fa060eba52e3e90be100bd1d8183811657
Paweł Grabiński <pawelrgrabinski@gmail.com>
2024-04-09 19:46:44 +0000

    Add more solutions to Project 2

--- List 1/P2.R
+++ List 1/P2.R
@@ -34,5 +34,33 @@ qqnorm(parent_data[["MotherHeight"]])
 dev.off()
 
 png("List 1/parents_data.png")
-plot(parent_data["Weight"], parent_data["Length"])
+pairs(as.data.frame(parent_data))
 dev.off()
+
+
+
+
+
+
+decomposition <- eigen(covariance)
+decomposition
+
+reconstructed_matrix <- matrix(0, nrow=4, ncol=4)
+for (i in 1:4) {
+  partial_matrix <- decomposition$values[i] * decomposition$vectors[,i] %*% t(decomposition$vectors[,i])
+  print(partial_matrix)
+  reconstructed_matrix <- reconstructed_matrix + partial_matrix
+}
+
+print(covariance - reconstructed_matrix)
+print(round(covariance - reconstructed_matrix, 9))
+
+
+parent_data_ptransformed <- as.data.frame(as.matrix(parent_data) %*% decomposition$vectors)
+parent_data_ptransformed
+
+ggplot() +
+  geom_point(data = parent_data_ptransformed, aes(x = V1, y = V2, color=parent_labels), size = 1, shape = 21) +
+  scale_color_manual(values = color_mapping) +
+  theme_bw()
+ggsave("List 1/deentanlged_parent.png", width = 6, height = 4, units = "in")
