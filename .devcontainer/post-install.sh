14b33caaaa7331a263bbfb9340b524304903f8d5
Paweł Grabiński <pawelrgrabinski@gmail.com>
2024-04-24 17:07:44 +0000

    Add latex installation

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -3,3 +3,6 @@ set -ex
 
 git config --global user.name "Paweł Grabiński"
 git config --global user.email "pawelrgrabinski@gmail.com"
+
+sudo apt-get update
+sudo apt-get install -y texlive-latex-base texlive-latex-extra
