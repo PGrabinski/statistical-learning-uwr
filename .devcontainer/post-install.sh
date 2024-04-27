41bd06ce27099cd5480f529fff9edccc76ed0994
Paweł Grabiński <pawelrgrabinski@gmail.com>
2024-04-27 12:35:42 +0000

    Move latex install to dockerfille

--- .devcontainer/post-install.sh
+++ .devcontainer/post-install.sh
@@ -3,6 +3,3 @@ set -ex
 
 git config --global user.name "Paweł Grabiński"
 git config --global user.email "pawelrgrabinski@gmail.com"
-
-sudo apt-get update
-sudo apt-get install -y texlive-latex-base texlive-latex-extra
